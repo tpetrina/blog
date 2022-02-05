9a01d4d4f423dfd6c961e085963168ec2e096065
Toni Petrina <tonipetrina@hotmail.com>
2022-02-05 20:53:01 +0100

    chore: Use top level async

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -2,60 +2,56 @@ import { writeFileSync } from "fs";
 import { globby } from "globby";
 import prettier from "prettier";
 
-async function generate() {
-  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
-  const pages = (
-    await globby([
-      "pages/**/*.tsx",
-      "!pages/_*.tsx",
-      "!pages/api",
-      "!pages/404.tsx",
-    ])
-  )
-    .filter((page) => page.indexOf("[") === -1)
-    .map((page) => page.replace(/(.*?)\/index.tsx/, "$1.tsx"));
+const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
+const pages = (
+  await globby([
+    "pages/**/*.tsx",
+    "!pages/_*.tsx",
+    "!pages/api",
+    "!pages/404.tsx",
+  ])
+)
+  .filter((page) => page.indexOf("[") === -1)
+  .map((page) => page.replace(/(.*?)\/index.tsx/, "$1.tsx"));
 
-  const posts = await globby(["data/posts/**/*.md"]);
-  console.log(posts);
+const posts = await globby(["data/posts/**/*.md"]);
+console.log(posts);
 
-  const urls = pages
-    .map((page) => {
-      const path = page.replace("pages", "").replace(".tsx", "");
-      const route = path === "/index" ? "" : path;
+const urls = pages
+  .map((page) => {
+    const path = page.replace("pages", "").replace(".tsx", "");
+    const route = path === "/index" ? "" : path;
 
-      return `
+    return `
         <url>
             <loc>${`https://tpetrina.com${route}`}</loc>
         </url>
       `;
-    })
-    .concat(
-      posts.map((post) => {
-        const path = post.replace(/^data\/posts\//, "/blog/").replace(".md", "");
-        const route = path;
-        return `
+  })
+  .concat(
+    posts.map((post) => {
+      const path = post.replace(/^data\/posts\//, "/blog/").replace(".md", "");
+      const route = path;
+      return `
       <url>
           <loc>${`https://tpetrina.com${route}`}</loc>
       </url>
     `;
-      })
-    )
-    .join("");
+    })
+  )
+  .join("");
 
-  const sitemap = `
+const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${urls}
     </urlset>
     `;
 
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: "html",
-  });
+const formatted = prettier.format(sitemap, {
+  ...prettierConfig,
+  parser: "html",
+});
 
-  // eslint-disable-next-line no-sync
-  writeFileSync("public/sitemap.xml", formatted);
-}
-
-generate();
+// eslint-disable-next-line no-sync
+writeFileSync("public/sitemap.xml", formatted);
