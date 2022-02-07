32d1d7c4008b6762cd2be60dd951cbab7d8ea6e2
Toni Petrina <tonipetrina@hotmail.com>
2022-02-07 23:08:44 +0100

    chore: Fix build and use preact in prod

--- next.config.js
+++ next.config.js
@@ -9,4 +9,15 @@ module.exports = withMDX({
 })({
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "mdx"],
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 });
