8dbb3d63344d6860f47c713fc8ae6a42e5eea273
Toni Petrina <tonipetrina@hotmail.com>
2023-01-24 22:21:15 +0100

    chore: Remove Preact

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
+// const withPreact = require("next-plugin-preact");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "mdx"],
   productionBrowserSourceMaps: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 };
