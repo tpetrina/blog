ea139917806f2d9674cb6a25c15bafa3af29c38b
Toni Petrina <tonipetrina@hotmail.com>
2023-05-31 13:39:27 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -60,7 +60,7 @@ export default function Footer() {
               key={item.name}
               href={item.href}
               className="text-gray-400 hover:text-gray-500"
-              rel={!!item.rel ? item.rel : null}
+              rel={!!item.rel ? item.rel : undefined}
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
