cc2f619c871a35ba9ca60ac6505b99759c09906d
Toni Petrina <tonipetrina@hotmail.com>
2025-01-03 21:44:19 +0100

    Support YAML

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -3,6 +3,7 @@ import "prismjs";
 import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-yaml";
 import { PropsWithChildren, useEffect, useRef } from "react";
 
 export function CodeBlock({
